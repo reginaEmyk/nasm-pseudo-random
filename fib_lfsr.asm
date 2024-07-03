2bd52eeb0897a6da37987aeb741a74aa6016e44e
renk <r_enk@outlook.com>
2024-07-03 02:12:37 -0300

    xorshift not working, mb cus sugned? never returning to initial state

--- fib_lfsr.asm
+++ fib_lfsr.asm
@@ -21,42 +21,37 @@ main: ; _start is in stdlib with printf
     ; bit is in edx
     ; period is in ecx
 ; loads random seed to eax and edx 32 32
-    RDTSC ; start state is a pseudo random seed from clock time in eax. a val from 0 to   
+   ; RDTSC ; start state is a pseudo random seed from clock time in eax. a val from 0 to   
         ; Test if EAX is negative
     ; start state is the last bit of seed, not 1 as in start_state = 1 << 15 | 1
     ; mov eax, 1 << 15 | 1
 
-    loop_RDTSC:
-        RDTSC
-        cmp eax, 1    ; Compare eax with 1
-        jl loop_RDTSC ; Jump to loop_RDTSC if EAX is less than 1 
+    ; loop_RDTSC:
+    ;     RDTSC
+    ;     cmp eax, 1    ; Compare eax with 1
+    ;     jl loop_RDTSC ; Jump to loop_RDTSC if EAX is less than 1 
     
-    ;mov eax, 1 << 15 | 1
-    shr eax, 16
-    and eax, 0xFFFF ; 16bit only
-    or eax, 1 ; RDTSC << 16 | 1
+    mov eax, 1 << 15 | 1
+    ; shr eax, 16
+    ; and eax, 0xFFFF ; 16bit only
+    ; or eax, 1 ; RDTSC << 16 | 1
 
-    mov eax, ebx ; start_state is in ebx
-    ;mov ebx, eax ; start_state is in ebx
-
+   
         ; Code to handle the case where EAX is negative
-  shr eax, 16  ; Shift right by 16 (might not be portable across architectures)
+;   shr eax, 16  ; Shift right by 16 (might not be portable across architectures)
 ; and eax, 0xFFFF  ; 
     ; lfsr = start_state
-    mov edx, 0 ; bit
+    mov edx, 0 ; helper
     mov ecx, 0 ; period
-    push eax ; start state on stack
-    mov ebp, esp  
-    ; cmp eax, [ebp]
-    ; je hey
+    
 
-    ; lea ebp, [esp]
+    mov ebx, eax ; for shift ops
     mov edx, eax
     mov ebp, eax ; save start state in ebp
     
 
-    ; need eax to b free
-    loop_lfsr : ; while true
+    
+    loop_lfsr : ; john metcalf tripets xorshift
     ; taps: 16 15 13 4; feedback polynomial: x^16 + x^15 + x^13 + x^4 + 1
     ; bit (lfsr ^ (lfsr >> 1) ^ (lfsr >> 3) ^ (lfsr >> 12)) & 1
     ; lfsr (lfsr >> 1) | (bit << 15)
@@ -68,28 +63,15 @@ main: ; _start is in stdlib with printf
     ; bit is in edx
     ; period is in ecx
 
-    push eax
-    mov ebx, eax ; start_state in ebx
-    shr eax, 0 ;(lfsr >> 0)
-    shr ebx, 2 ;(lfsr >> 2)
-    xor ebx, eax ;(lfsr >> 0) ^ (lfsr >> 2)
-    shr eax,3 ;(lfsr >> 3)
-    xor edx, eax ; (lfsr >> 0) ^ (lfsr >> 2)^ (lfsr >> 3)
-    shr eax,2 ;(lfsr >> 5)
-    xor edx, eax ; ((lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5))
-    and edx, 1; ((lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5)) & 1u
-; bit in edx is ((lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5)) & 1u;
 
-
-    pop  eax; eax = lfsr, stack: start_state
-    push edx; stack: bit, start_state
-
-    shr eax, 1 ; (lfsr >> 1)
-    shl edx, 15; (bit << 15)
-    or eax, edx; (lfsr >> 1) | (bit << 15)
-;lfsr in eax is lfsr = (lfsr >> 1) | (bit << 15)
-
-    pop edx ; bit in edx, stack: start_state
+    
+    shr eax, 7 ;(lfsr >> 7)
+    xor edx, eax ; (lfsr ) ^ (lfsr >> 7)
+    shl eax, 9 ;(lfsr << 9)
+    xor edx, eax ; (lfsr ) ^ (lfsr >> 7) ^ (lfsr << 9)
+    shr eax, 13 ;(lfsr >> 13)
+    xor edx, eax ; (lfsr ) ^ (lfsr >> 7) ^ (lfsr << 9) ^ (lfsr >> 13)
+    mov eax, edx ; lfsr = start_state
 
     inc ecx ;period += 1
 
@@ -104,11 +86,7 @@ print_int:
 push ecx              ; push period onto the stack
 push int_format       ; push format string onto the stack
 call printf           ; call printf
-
 add esp, 8            ; clean up the stack (2 * 4 bytes)
-mov eax, 1 ; System call number for exit
-mov ebx, 0 ; Exit status (0 for success)
-int 0x80   ; Interrupt for system call
 
 exit:
 ; call print_inte
