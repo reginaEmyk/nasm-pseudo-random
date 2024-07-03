fd6da30b80b81a0a9f70706f2167f30d4cfee516
renk <r_enk@outlook.com>
2024-07-03 03:43:20 -0300

    fib working as well

--- fib_lfsr.asm
+++ fib_lfsr.asm
@@ -21,69 +21,87 @@ main: ; _start is in stdlib with printf
     ; bit is in edx
     ; period is in ecx
 ; loads random seed to eax and edx 32 32
-   ; RDTSC ; start state is a pseudo random seed from clock time in eax. a val from 0 to   
-        ; Test if EAX is negative
-    ; start state is the last bit of seed, not 1 as in start_state = 1 << 15 | 1
-    ; mov eax, 1 << 15 | 1
-
-    loop_RDTSC:
-        RDTSC
-        cmp eax, 0    ; Compare eax with 1
-        je loop_RDTSC ; Jump to loop_RDTSC if EAX is less than 1 
+    loop_seed:
+        RDTSC ; start state is a pseudo random seed from clock time in eax. a val from 0 to 
+        shr eax,16
+        cmp eax,0
+        je loop_seed  
+        
     
+    mov ebx, eax ; start_state is in ebx
+
+        ; Code to handle the case where EAX is negative
+
     ; lfsr = start_state
-    
-    mov edx, 0 ; helper
+    mov edx, 0 ; bit
     mov ecx, 0 ; period
-    
+    push eax ; start state on stack
+    mov ebp, esp  
+    ; cmp eax, [ebp]
+    ; je hey
 
-    mov ebx, eax ; for shift ops
+    ; lea ebp, [esp]
     mov edx, eax
     mov ebp, eax ; save start state in ebp
     
 
-    
-    loop_lfsr : ; john metcalf tripets xorshift
+    ; need eax to b free
+    loop : ; while true
     ; taps: 16 15 13 4; feedback polynomial: x^16 + x^15 + x^13 + x^4 + 1
     ; bit (lfsr ^ (lfsr >> 1) ^ (lfsr >> 3) ^ (lfsr >> 12)) & 1
     ; lfsr (lfsr >> 1) | (bit << 15)
     ; period period + 1
     
     
-    ; start_state is in ebp
+    ; start_state is in ebx
     ; lfsr is in aex
     ; bit is in edx
     ; period is in ecx
 
+    push eax
+    mov ebx, eax ; start_state in ebx
+    mov edx, eax
 
+    shr eax, 1 ;(lfsr >> 1)
+    xor edx, eax ;lfsr ^ (lfsr >> 1) 
+
+    shr eax,2 ;(lfsr >> 3)
+    xor edx, eax ; lfsr ^ (lfsr >> 1) ^ (lfsr >> 3)
+
+    shr eax,9 ;(lfsr >> 12)
+    xor edx, eax ; lfsr ^ (lfsr >> 1) ^ (lfsr >> 3) ^ (lfsr >> 12))
     
-    shl eax, 13 ;(lfsr << 7)
-    xor edx, eax ; (lfsr ) ^ (lfsr << 7)
-    mov eax, edx ; 
-
-    shr eax, 17 ;(lfsr >> 9)
-    xor edx, eax ; (lfsr ) ^ (lfsr << 7) ^ (lfsr >> 9)
-    mov eax, edx ; 
-
-    shl eax, 5 ;(lfsr << 13)
-    xor edx, eax ; (lfsr ) ^ (lfsr << 7) ^ (lfsr >> 9) ^ (lfsr >> 13)
-    mov eax, edx ; lfsr = start_state
+    and edx, 1; (lfsr ^ (lfsr >> 1) ^ (lfsr >> 3) ^ (lfsr >> 12)) & 1
+; bit in edx is (lfsr ^ (lfsr >> 1) ^ (lfsr >> 3) ^ (lfsr >> 12)) & 1
 
 
-    ; cmp ecx, 0xFFFFFFFF  ; Check if ecx is at its maximum signed value
-    ; je print_int    ; Jump if not equal (no overflow)
+    pop  eax; eax = lfsr, stack: start_state
+    push edx; stack: bit, start_state
 
-    cmp eax, ebp ; if start_state == lfsr 
-    je print_int ; then exit loop_lfsr and print period
+    shr eax, 1 ; (lfsr >> 1)
+    shl edx, 15; (bit << 15)
+    or eax, edx; (lfsr >> 1) | (bit << 15)
+;lfsr in eax is lfsr = (lfsr >> 1) | (bit << 15)
 
-    jmp loop_lfsr
+    pop edx ; bit in edx, stack: start_state
+
+    inc ecx ;period += 1
+
+    cmp ebp, eax ; if start_state == lfsr 
+    je print_int ; then exit loop and print period
+
+    jmp loop
 
 ; http://pacman128.github.io/static/pcasm-book.pdf
 print_int: 
 push eax              ; push period onto the stack
 push int_format       ; push format string onto the stack
 call printf           ; call printf
+
 add esp, 8            ; clean up the stack (2 * 4 bytes)
+mov eax, 1 ; System call number for exit
+mov ebx, 0 ; Exit status (0 for success)
+int 0x80   ; Interrupt for system call
 
 exit:
 ; call print_inte
