9d84a18005d818051514ab5e28a3bea663cc74be
renk <r_enk@outlook.com>
2024-07-03 03:54:02 -0300

    code cleanse

--- fib_lfsr.asm
+++ fib_lfsr.asm
@@ -6,88 +6,68 @@ section .data
     
 
 section .text
-; Program entry point
-global main
-extern printf
+    global main
+    extern printf
+
+    main:  
+
 ; https://stackoverflow.com/questions/17182182/how-to-create-random-number-in-nasm-getting-system-time
+        loop_RDTSC:
+            RDTSC
+            cmp eax, 0  
+            je loop_RDTSC ; seed can't be 0
+        
+        ; lfsr = start_state
+        
+        mov edx, 0 ;
+        mov ecx, 0 ; period
+        
 
-global main
-main: ; _start is in stdlib with printf 
-    ; get seed from current time
-    
-    
-    ; start_state is in ebx
-    ; lfsr is in aex
-    ; bit is in edx
-    ; period is in ecx
-; loads random seed to eax and edx 32 32
-   ; RDTSC ; start state is a pseudo random seed from clock time in eax. a val from 0 to   
-        ; Test if EAX is negative
-    ; start state is the last bit of seed, not 1 as in start_state = 1 << 15 | 1
-    ; mov eax, 1 << 15 | 1
+        mov ebx, eax ; for shift operations
+        mov edx, eax
+        mov ebp, eax ; save start state in ebp
+        
 
-    loop_RDTSC:
-        RDTSC
-        cmp eax, 0    ; Compare eax with 1
-        je loop_RDTSC ; Jump to loop_RDTSC if EAX is less than 1 
-    
-    ; lfsr = start_state
-    
-    mov edx, 0 ; helper
-    mov ecx, 0 ; period
-    
+        
+        loop_lfsr : ; john metcalf tripets xorshift
+        ; taps: 16 15 13 4; feedback polynomial: x^16 + x^15 + x^13 + x^4 + 1
+        ; bit (lfsr ^ (lfsr >> 1) ^ (lfsr >> 3) ^ (lfsr >> 12)) & 1
+        ; lfsr (lfsr >> 1) | (bit << 15)
+        ; period period + 1
+        
+        
+        ; start_state is in ebp
+        ; lfsr is in aex
+        ; bit is in edx
+        ; period is in ecx
 
-    mov ebx, eax ; for shift ops
-    mov edx, eax
-    mov ebp, eax ; save start state in ebp
-    
+        
+        shl eax, 13 ;(lfsr << 7)
+        xor edx, eax ; (lfsr ) ^ (lfsr << 7)
+        mov eax, edx ; 
 
-    
-    loop_lfsr : ; john metcalf tripets xorshift
-    ; taps: 16 15 13 4; feedback polynomial: x^16 + x^15 + x^13 + x^4 + 1
-    ; bit (lfsr ^ (lfsr >> 1) ^ (lfsr >> 3) ^ (lfsr >> 12)) & 1
-    ; lfsr (lfsr >> 1) | (bit << 15)
-    ; period period + 1
-    
-    
-    ; start_state is in ebp
-    ; lfsr is in aex
-    ; bit is in edx
-    ; period is in ecx
+        shr eax, 17 ;(lfsr >> 9)
+        xor edx, eax ; (lfsr ) ^ (lfsr << 7) ^ (lfsr >> 9)
+        mov eax, edx ; 
+
+        shl eax, 5 ;(lfsr << 13)
+        xor edx, eax ; (lfsr ) ^ (lfsr << 7) ^ (lfsr >> 9) ^ (lfsr >> 13)
+        mov eax, edx ; lfsr = start_state
 
 
-    
-    shl eax, 13 ;(lfsr << 7)
-    xor edx, eax ; (lfsr ) ^ (lfsr << 7)
-    mov eax, edx ; 
+        cmp eax, ebp ; if start_state == lfsr 
+        je print_int ; exit loop_lfsr and print period
 
-    shr eax, 17 ;(lfsr >> 9)
-    xor edx, eax ; (lfsr ) ^ (lfsr << 7) ^ (lfsr >> 9)
-    mov eax, edx ; 
+        jmp loop_lfsr
 
-    shl eax, 5 ;(lfsr << 13)
-    xor edx, eax ; (lfsr ) ^ (lfsr << 7) ^ (lfsr >> 9) ^ (lfsr >> 13)
-    mov eax, edx ; lfsr = start_state
+    ; http://pacman128.github.io/static/pcasm-book.pdf
+    print_int: 
+        push eax              ; push period onto the stack
+        push int_format       ; push format string onto the stack
+        call printf           ; call printf
+        add esp, 8            ; clean up the stack (2 * 4 bytes)
 
-
-    ; cmp ecx, 0xFFFFFFFF  ; Check if ecx is at its maximum signed value
-    ; je print_int    ; Jump if not equal (no overflow)
-
-    cmp eax, ebp ; if start_state == lfsr 
-    je print_int ; then exit loop_lfsr and print period
-
-    jmp loop_lfsr
-
-; http://pacman128.github.io/static/pcasm-book.pdf
-print_int: 
-push eax              ; push period onto the stack
-push int_format       ; push format string onto the stack
-call printf           ; call printf
-add esp, 8            ; clean up the stack (2 * 4 bytes)
-
-exit:
-; call print_inte
-; Exit program (syscall for Linux/OSX)
-mov eax, 1 ; System call number for exit
-mov ebx, 0 ; Exit status (0 for success)
-int 0x80   ; Interrupt for system call
+    exit:
+        mov eax, 1 ; System call number for exit
+        mov ebx, 0 ; Exit status (0 for success)
+        int 0x80   ; Interrupt for system call
