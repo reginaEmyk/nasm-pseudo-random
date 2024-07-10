818684fc1080bc3211786101089cc63b0ac119d2
renk <r_enk@outlook.com>
2024-07-09 22:12:51 -0300

    simple frequence: prints amounts of ocurrences by 3 groups separated by expected/observed

--- chi.c
+++ chi.c
@@ -23,7 +23,7 @@ int get_polynomial(int lfsr){
     bit = (bit << 23); // todo checl if this ever above 24
 
     if (bit >= array_size){
-        printf("bit is bigger than array size");
+        // printf("bit is bigger than array size");
         exit(1);
     }
 
@@ -53,6 +53,7 @@ int* _lfsr(__int32_t);
 // df = 1. df = k - 1. k = 2. k is amount of categories: uniform and not uniform.
 double chiUniformExpected(int* pseudoRandom);
 double chiUniformExpected(int* pseudoRandom){
+    // printf("Initiating chi square test... \n");
     double chi_sqr = 0;  
     double* chi_sqr_classes = (double*)malloc(n_classes * sizeof(double)); 
     int pseudo = -1;
@@ -61,6 +62,13 @@ double chiUniformExpected(int* pseudoRandom){
     int class_lowest = 0; // todo uniform typing
     int class_highest = 0;
     int pseudoOcurr = 0;
+
+// counts to print frequencies as output txt file is too big
+    int expNotObs = 0;
+    int expOverObs = 0;
+    int expObs = 0;
+    int notExpObs = 0;
+    int countOther = 0;
 
 // allocating classes of pseudo randoms and their ocurrences
 // classes[classIdx][idx], has pseudo and ocurrence. classes->class->index->pseudo|ocurrence.
@@ -84,50 +92,76 @@ double chiUniformExpected(int* pseudoRandom){
             index = i;
             classes[MClass][index][0] = pseudoRandom[index]; // put pseudo in class
             classes[MClass][index][1] += 1; // increase counter of ocurrence of pseudo in this class. malloc ensures it starts at 0
-            // printf(" %i", index);
         }
         
     }
 
 
 
-// get chi_sqr
+// get chi_sqr and counts
     for (int classIdx = 0; classIdx < n_classes; classIdx++) // 16 classes
     {
         class_lowest = classIdx * array_size/n_classes;
         class_highest = classIdx * (array_size + 1)/n_classes - 1;
-        for (int index = 0, i = 0; i < array_size/n_classes; i++) // iterate through each class without separating pseudos 
+        expNotObs = 0;     expOverObs = 0;     expObs = 0;     notExpObs = 0;   countOther = 0;
+        printf("class %i [%i...%i]; \n", classIdx, class_lowest, class_highest);
+
+        for (int index = 0, i = 0; index < array_size/n_classes; index++) // iterate through each class without separating pseudos 
         {
             // a number is expected to be in a class of uniform distribution when
             // it's within the bounds of the class, and exp freq is UNIFORM_FREQ;
             // otherwise exp freq is 0
-            pseudo = classes[classIdx][index][0];
-            pseudoOcurr = (double)1/classes[classIdx][index][1];
+            pseudo = classes[classIdx][index][0]; // value of pseudo
+            pseudoOcurr = (double)1/classes[classIdx][index][1]; // ocurrences of pseudo in class 
 
-            if(pseudoOcurr == 0)
-                pseudoOcurr = ALMOST_ZERO_DENOMINATOR;
+
+            switch (pseudoOcurr)
+            {
+            case 0:
+                expNotObs++;
+                break;
+            case 1:
+                expObs++;
+                break;
+            default:
+                countOther++;
+                break;
+            }
 
             obs_freq = (double)pseudoOcurr/CLASS_SIZE;
 
-            if(pseudo < class_lowest || pseudo > class_highest) 
+            if(pseudo < class_lowest || pseudo > class_highest){
                 expected_freq = ALMOST_ZERO_DENOMINATOR; // cant have 0 denominator
-            else
+                notExpObs++; // number was not expected in this class but was observed
+            }
+            else{
                 expected_freq = UNIFORM_FREQ;
-
-
+                expObs++; // number was expected and observed within group
+            }
+            
+            // printf("[%i]; pseudo random: %i; obs_freq: %f; expected_freq: %f; \n", classIdx, pseudo, obs_freq, expected_freq);
             chi_sqr += (obs_freq - expected_freq) * (obs_freq - expected_freq) / (expected_freq);
         }
+        printf(" Expected and Observed: %i; Expected and Not Observed: %i; Not Expected and Observed: %i", expObs, expNotObs, notExpObs);
         
     }
+    // printf("Exiting chi square test... \n");
 
     return chi_sqr;
 }
 
 
+// void printFrequency(int* pseudoRandom, int* frequency);
+// void printFrequency(int* pseudoRandom, int* frequency){
+//     int pseudo = -1;
+//     int pseudoOcurr = 0;
+//     int class_lowest = 0; // todo uniform typing
+//     int class_highest = 0;
+
+//     for 
+// }
 
 int main(){
-    // printf("Hello, World!");
-    // 16777216
     int* nasmPseudoRandom = (int*)malloc(array_size * sizeof(int));
     int* cPseudoRandom = (int*)malloc(array_size * sizeof(int));
     int* pseudR_frequency = (int*)malloc(array_size * sizeof(int));
@@ -139,102 +173,79 @@ int main(){
     clock_t clock_nasm, clock_c ;  // function execution takes less than a second, must use clock in ANSI C. https://stackoverflow.com/questions/361363/how-to-measure-time-in-milliseconds-using-ansi-c.
     double chiSquare = 0;
     int areBothEqual = 0;
+    int hasRepeated = 0;
 
-    printf("calling 16777215 \n;");
+    
     clock_nasm = clock();
+    printf("Calling LFSR in nasm, SEED: %i ...\n", SEED);
     nasmPseudoRandom = _lfsr(SEED);
     clock_nasm = clock() - clock_nasm;
 
     clock_c = clock();
+    printf("Calling LFSR in c, SEED: %i ...\n", SEED);
     cPseudoRandom = lfsr_array(cPseudoRandom, SEED);
     clock_c = clock() - clock_c;
-// todo fix:   WRONG  index 16777215 nasm is 8388608 c is 0
-// todo test for both arrays
-    chiSquare = chiUniformExpected(cPseudoRandom);
-    printf("chi_square %f ", chiSquare);
-    if(chiSquare > CRITICAL_VALUE){
-        printf(" REJECTED hypothesis, %f > %f . Numbers are NOT uniformly distributed.chi square > critical value for alpha = 0.05 and df 1. (chi dist table https://people.smp.uq.edu.au/YoniNazarathy/stat_models_B_course_spring_07/distributions/chisqtab.pdf)\n ", chiSquare, CRITICAL_VALUE);
-    } else{
-        printf(" ACEPTED hypothesis, %f <= %f . Numbers are uniformly distributed.chi square <= critical value for alpha = 0.05 and df 1. (chi dist table https://people.smp.uq.edu.au/YoniNazarathy/stat_models_B_course_spring_07/distributions/chisqtab.pdf)\n", chiSquare, CRITICAL_VALUE);
-    }
 
     for (int i = 0; i < array_size; i++)
     {
         if(nasmPseudoRandom[i] != cPseudoRandom[i]){
-            printf(" WRONG  index %i nasm is %i c is %i", i, nasmPseudoRandom[i], cPseudoRandom[i] );
+            printf(" Do both nasm and c implementations produce the same values? NO!  The LFSR differ!  index %i nasm is %i c is %i", i, nasmPseudoRandom[i], cPseudoRandom[i] );
             areBothEqual = 1;
             break;
         } 
     }
 
-    if (areBothEqual == 0)
-        printf("The implementations for c and nasm gave the same resulting array. \n");
+    if (areBothEqual == 0){
+        printf(" Do both nasm and c implementations produce the same values? YES! The implementations for c and nasm gave the same resulting array. \n");
+// since both are known to be equal and the output is huge, only reporting for one lfsr
+        chiSquare = chiUniformExpected(cPseudoRandom);
+        printf("chi_square %f \n", chiSquare);
+        if(chiSquare > CRITICAL_VALUE){
+            printf(" REJECTED hypothesis, chi-square: %f > %f (critical value). Numbers are NOT uniformly distributed.chi square > critical value for alpha = 0.05 and df 1. (chi dist table https://people.smp.uq.edu.au/YoniNazarathy/stat_models_B_course_spring_07/distributions/chisqtab.pdf)\n ", chiSquare, CRITICAL_VALUE);
+        } else{
+            printf(" ACEPTED hypothesis, chi-square: %f <= %f (critical value). Numbers are uniformly distributed.chi square <= critical value for alpha = 0.05 and df 1. (chi dist table https://people.smp.uq.edu.au/YoniNazarathy/stat_models_B_course_spring_07/distributions/chisqtab.pdf)\n", chiSquare, CRITICAL_VALUE);
+    }
 
-    
-// todo print arrays
-// todo determine acceptance rejection regions?
-
-
-    // for (int i = 0; i < array_size; i++)
-    // {
-    //     printf(" %i; ", pseudoRandom[i]);
-    // }
-
-
-// todo chi's region of acceptance/rejection?
-
-
-    
+}
+   
     printf("\n time taken by nasm: %ld clocks, %ld secs", clock_nasm, clock_nasm/CLOCKS_PER_SEC);
     printf("\n time taken by c: %ld clocks, %ld secs \n", clock_c, clock_c/CLOCKS_PER_SEC);
     printf("\n c/nasm (clocks): %f  \n", (double)clock_c/(double)clock_nasm);
 
+// extra report functions 
 
+// print generated numbers
+    // printf("Printing Pseudo randoms generated with nasm function... \n");
+    //  for (int i = 0; i < array_size; i++)
+    //  {
+    //     printf(" %i ", nasmPseudoRandom[i]);
+    //  }
+    // printf("Printed Pseudo randoms generated with nasm function. \n");
 
-    // printf("\n finished printing 16777215");
+    // printf("Printing Pseudo randoms generated with c function... \n");
+    //  for (int i = 0; i < array_size; i++)
+    //  {
+    //     printf(" %i ", cPseudoRandom[i]);
+    //  }
+    // printf("Printed Pseudo randoms generated with c function. \n");
+
+// // check if any nu,bers were repeated
     // for (int i = 0; i < array_size; i++)
     // {
-    //     printf(" %i; ", pseudoRandom[i]);
-    // }
-
-    // printf("\n getting chi_sqr index");
-    // // getting chi_sqr index
-    // qsort(pseudoRandom, array_size, sizeof(int), comp);
-
-    // printf("\n sorted array");
-    // for (int i = 0; i < array_size; i++)
-    // {
-    //     if(i < array_size - 1 && pseudoRandom[i] == pseudoRandom[i+1] ){
-    //         for (count_ocurr = 1; count_ocurr <= array_size, pseudoRandom[i] == pseudoRandom[i+1] ; count_ocurr++, i++);
-    //         chi_sqr += (freq_ob - freq_exp )*(freq_ob - freq_exp )/(freq_exp);
-    //     } else{
-    //         chi_sqr += (1 - freq_exp )*(freq_ob - freq_exp )/(freq_exp);
+    //     for (int j = 0; j < array_size; j++)
+    //     {
+    //         if( i != j && nasmPseudoRandom[i] == nasmPseudoRandom[j]){
+    //              printf("Repeated number %i found in nasmPseudoRandom at indexes %i,  and %i", nasmPseudoRandom[j], i, j);
+    //              hasRepeated = 1;
+    //         }
     //     }
+        
     // }
-    // printf("\n chi_sqr index: %f\n", chi_sqr);
+    // if (hasRepeated == 0){
+    //     printf("No repeated numbers found in nasmPseudoRandom. \n");
+    // }
     
     
 
-    //chi square i sum of (realFreq - expFreq)**2/(expFreq)
-   
-
-    // todo: make a huge 2^24 -1 vector calling lfsr
-    // todo: chi test it
-
-    // Set of unsigned 24 bit numbers: from 1 to 16777215, 
-    // each is expected to happen 1/16777215 times
-
-    // 1M is 1048576 = 2^20
-
-    // any number in any class is expected to happen 1/2^20 times per 1M class
-
-    // generate 16M numbers
-    // sort
-    // get frequency for all classes ( (0, 1M], (1M,2M]... )
-        // Expected frequency of each number is 1/1M
-        // Observed freq is <n ocurrences>/1M
-    // Calculate chi squared estimator
-
-    
     return 0;
 }
